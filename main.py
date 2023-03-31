9af5281cb0ce7f5938490ebda8169daa66049afc
Matt Freed <mwf58@cornell.edu>
2023-03-31 16:18:26 -0400

    Formatting

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-## intitial prompt here
+#Run this code!
+
+import argparse
+from src import promptmixer, img2prompt, prompt2img
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate images based on an initial prompt")
+    parser.add_argument("starting_prompt", help="Your starting prompt")
+    parser.add_argument("output_dir", help="Path to the directory where the generated images will be saved.")
+    args = parser.parse_args()
+
+    prompt2img.generate_images(args.input_file, args.output_dir)
